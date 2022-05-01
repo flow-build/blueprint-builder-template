78e74a33337216af476556aa5cad8d73a8ace351
Clerivaldo Junior <clerivaldo.junior@fdte.io>
2022-05-01 16:35:02 -0300

    feat(wip): Adicionados nós para buscar usuário; Aguardando implementação do serviço

--- src/nodes/specs/authResponse.js
+++ src/nodes/specs/authResponse.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "AUTH RESPONSE",
   type: "Flow",
   next: {
-    OK: "GENERATE-TOKEN",
+    OK: "GENERATE-TOKEN", // Trocar para "GET-USER-BY-ID" quando o serviço estiver pronto
     default: "NOTIFY-USER"
   },
   lane_id: "sessionId",
