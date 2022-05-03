dd0a9c9ccdc435a51e239e41eade494da5f54a00
Gustavo Haramura <gustavo.haramura@fdte.io>
2022-05-03 07:40:03 -0300

    refactor: bundle startPayment node spec

--- src/nodes/specs/startPayment.js
+++ src/nodes/specs/startPayment.js
@@ -1,16 +1,37 @@
-module.exports = {
-  id: "START-PAYMENT",
-  name: "START",
-  type: "Start",
-  next: "END",
-  lane_id: "free",
-  parameters: {
-    input_schema: {
-      type: "object",
-      required: ["orderId"],
-      properties: {
-        orderId: { type: "string", format: "uuid" },
+module.exports = [
+  {
+    id: "START-PAYMENT",
+    name: "START",
+    type: "Start",
+    next: "END",
+    lane_id: "free",
+    parameters: {
+      input_schema: {
+        type: "object",
+        required: ["orderId"],
+        properties: {
+          orderId: { type: "string", format: "uuid" },
+        },
       },
     },
   },
-};
+  {
+    id: "CONFIG",
+    name: "CONFIG",
+    next: "END",
+    type: "SystemTask",
+    category: "setToBag",
+    lane_id: "free",
+    parameters: {
+      input: {
+        actor: {
+          actorId: { $ref: "actor_data.actor_id" },
+          sessionId: { $ref: "actor_data.session_id" },
+        },
+        order: {
+          id: { $ref: "bag.orderId"}
+        }
+      },
+    },
+  },
+];
